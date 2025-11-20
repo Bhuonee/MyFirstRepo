5c3c1daec5853d2577d761f47f964da1847c9203
Bhuonee <canbeusedtoping@gmail.com>
2025-11-20 14:43:44 +0530

    resolved the issue

--- main.cpp
+++ main.cpp
@@ -4,11 +4,9 @@ int main()
 {
 	std::cout "nothing\n";
 	std::cout << "i added a new one" << std::endl;
-<<<<<<< HEAD
+
 	std::cout << "remove this line" << std::endl;
-=======
-	std::cout << "keep this line" << std::endl;
->>>>>>> 3e997a8c49110697ba32f07b974982bd69a772a5
+
 
 	return 0;
 } 
